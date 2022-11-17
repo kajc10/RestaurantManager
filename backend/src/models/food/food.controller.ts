62307e38cf5bd3f5ca218d3b18ae0783c5957977
Szűr Márton <szur.marton@metcont.co>
2022-11-17 18:55:37 +0100

    Add decorator to controllers

--- backend/src/models/food/food.controller.ts
+++ backend/src/models/food/food.controller.ts
@@ -1,32 +1,40 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { FoodService } from './food.service';
 import { FoodDto } from './dto/food.dto';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('food')
 @Controller('food')
 export class FoodController {
   constructor(private readonly foodService: FoodService) {}
 
   @Post()
+  @ApiBearerAuth()
   create(@Body() FoodDto: FoodDto) {
     return this.foodService.create(FoodDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: [FoodDto] })
+  @ApiBearerAuth()
   findAll() {
     return this.foodService.findAll();
   }
 
   @Get(':id')
+  @ApiBearerAuth()
   findOne(@Param('id') id: string) {
     return this.foodService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiBearerAuth()
   update(@Param('id') id: string, @Body() FoodDto: FoodDto) {
     return this.foodService.update(+id, FoodDto);
   }
 
   @Delete(':id')
+  @ApiBearerAuth()
   remove(@Param('id') id: string) {
     return this.foodService.remove(+id);
   }
--- backend/src/models/reservation/reservation.controller.ts
+++ backend/src/models/reservation/reservation.controller.ts
@@ -1,32 +1,40 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { ReservationDto } from './dto/reservation.dto';
 import { ReservationService } from './reservation.service';
 
+@ApiTags('reservation')
 @Controller('reservation')
 export class ReservationController {
   constructor(private readonly reservationService: ReservationService) {}
 
   @Post()
+  @ApiBearerAuth()
   create(@Body() ReservationDto: ReservationDto) {
     return this.reservationService.create(ReservationDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: [ReservationDto] })
+  @ApiBearerAuth()
   findAll() {
     return this.reservationService.findAll();
   }
 
   @Get(':id')
+  @ApiBearerAuth()
   findOne(@Param('id') id: string) {
     return this.reservationService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiBearerAuth()
   update(@Param('id') id: string, @Body() ReservationDto: ReservationDto) {
     return this.reservationService.update(+id, ReservationDto);
   }
 
   @Delete(':id')
+  @ApiBearerAuth()
   remove(@Param('id') id: string) {
     return this.reservationService.remove(+id);
   }
